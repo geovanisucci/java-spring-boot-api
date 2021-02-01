dc3f8b0325e617767a7c7402a9efacb2d03c77ff
geovanisucci <geovanis@ciandt.com>
2021-02-01 09:58:27 -0300

    JPQL e Query

--- forum/src/main/java/br/com/sample/forum/data/TopicoRepository.java
+++ forum/src/main/java/br/com/sample/forum/data/TopicoRepository.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.sample.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
 	List<Topico> findByCursoNome(String nomeCurso);
-
+	
+	@Query("SELECT t FROM Topico WHERE t.titulo = :titulo")
+	List<Topico> filtrarPorTitulo(@Param("titulo") String titulo);
 }
